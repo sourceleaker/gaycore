010eff81bfe48d71a24d8bb005d27dcb2d8be5ee
Krzysztof Kulczycki <kulczycki.k@gmail.com>
2011-11-13 00:48:17 +0100

    [4] Add readme/tutorial for sql updates. If you wanna see it, You should go to sql/ACORE3/updates/

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,6 +1,6 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
- #define ACORE_REVISION "3"
+ #define ACORE_REVISION "4"
  
  #define ACORE_DB_WORLD    "required_03_db_version"
  #define ACORE_DB_CHAR     "required_03_db_version"

f040d639676e9dc80f80b581cda32aebd3aeb06f
Krzysztof Kulczycki <kulczycki.k@gmail.com>
2011-11-14 14:59:00 +0100

    [6] Acore1: a1e9e61580195456a9b02aa541a7c0b431b4c01d

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
- #define ACORE_REVISION "5"
+ #define ACORE_REVISION "6"
  
- #define ACORE_DB_WORLD    "required_05_troll"
+ #define ACORE_DB_WORLD    "required_06_quest_template"
  #define ACORE_DB_CHAR     "required_03_db_version"
  #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

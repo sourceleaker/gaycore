1bedd7b7b31a6ff82ecd630ffdcd15c1814d9a75
kiper <kiperek@o2.pl>
2011-11-14 21:39:59 +0100

    [25]ACore1:Commit:1c7ecb63e6f42068aac8b55a858c1f9f76b659c8
    * Core/Quest: Add smart support for quest "Young and Vicious"
    Commit:18c3415cd8193bae279d99d549c6c41580d6bd05
    
    * Core/Quest: dalej nie działa ale mam dosyć
    
    Commit:1d501553f0bfaf358166db11001d693fea60a4b1
    
    * Fix quest Young and vicious (Armano you sux!)

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "24"
+#define ACORE_REVISION "25"
 
-#define ACORE_DB_WORLD    "required_24_corrupted_bloodtalon"
+#define ACORE_DB_WORLD    "required_25_Young_and_vicious"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

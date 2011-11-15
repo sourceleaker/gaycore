1efe2d725bcc0487aefcc4196d34201216e9c30c
kiper <kiperek@o2.pl>
2011-11-15 10:36:05 +0100

    [38]Acore1:Commit:d4be8d199c7ef1aa7425d748d4e820ecdc0fb177
    * DB/Creature: Add SAI for Scarlet Members in Trisfal Glades

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "37"
+#define ACORE_REVISION "38"
 
-#define ACORE_DB_WORLD    "required_37_quest_templates_trisfal_glades_and_silverpine_forest_for_horde"
+#define ACORE_DB_WORLD    "required_38_creature_scarlet_members_in_trisfal"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

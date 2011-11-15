4478e4f242088aa17c9d1f76e4bb58a9fa545d25
kiper <kiperek@o2.pl>
2011-11-15 12:03:26 +0100

    [43]Acore1:Commit:e4eb98f57ed379dbc1291954d8900aac33de875a
    * DB/Quest: [Trisfal Glades] Johaan's Experiment - add event after received reward by player

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "42"
+#define ACORE_REVISION "43"
 
-#define ACORE_DB_WORLD    "required_42_world_quest_13983"
+#define ACORE_DB_WORLD    "required_43_quest_johaans_experiment"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

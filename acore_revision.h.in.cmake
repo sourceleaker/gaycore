11f9374889ff9b96a23b27db5f8dcc7497c730fa
Jenova <Jenovas0@yahoo.com>
2011-11-15 23:47:48 +0100

    Revision Fix

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -2,7 +2,7 @@
 #define __ACORE_REVISION_H__
 #define ACORE_REVISION "72"
 
-#define ACORE_DB_WORLD    "required_72_Northern_Barrens_part2`"
+#define ACORE_DB_WORLD    "required_72_Northern_Barrens_part2"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

c0542fefcdf3e11f9ffeee48923a41b4c3200981
kiper <kiperek@o2.pl>
2011-11-15 12:23:36 +0100

    [49]Acore1:Commit:9f78c1b90ae895c1b4e8430522fde6e5215036ab
    * Duskwood Zone

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "48"
+#define ACORE_REVISION "49"
 
-#define ACORE_DB_WORLD    "required_48_world_npc_34512"
+#define ACORE_DB_WORLD    "required_49_duskwood"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

7b96f391e199077556e99f5ce7d8dabecacaaa2f
Krzysztof Kulczycki <kulczycki.k@gmail.com>
2011-11-15 15:22:56 +0100

    [57a] Change revision number, I can't change this in last commit - synch..error

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,6 +1,6 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "56"
+#define ACORE_REVISION "57"
 
 #define ACORE_DB_WORLD    "required_56_quests_wotlk_ships"
 #define ACORE_DB_CHAR     "required_03_db_version"

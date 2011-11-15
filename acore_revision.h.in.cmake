86d6a3486ef4b9e5c143924b87bda423722f3101
kiper <kiperek@o2.pl>
2011-11-15 18:08:51 +0100

    [58]Acore1:Commit:c0c6c0153fdf0ad2e51db3fb76b1926c0f7cee7b
    * DB/NPC: Block moveing "Primordial Drake Egg" (28408)
    
    DB/Npc: Block moveing "Webbed Crusader" (30273, 30268)

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "57"
+#define ACORE_REVISION "58"
 
-#define ACORE_DB_WORLD    "required_56_quests_wotlk_ships"
+#define ACORE_DB_WORLD    "required_58_wotlk_minor_fixes"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

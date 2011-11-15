2ee097b6728cd2a960e90b7157eb5520ccfe6509
kiper <kiperek@o2.pl>
2011-11-15 10:39:13 +0100

    [39]Acore1:Commit:17566571f97574d5472db57a6bda60a1005d0f68
    * DB/Quest: [Trisfal Glades] Variety is the Spice of Death now should be 100% efficient

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "38"
+#define ACORE_REVISION "39"
 
-#define ACORE_DB_WORLD    "required_38_creature_scarlet_members_in_trisfal"
+#define ACORE_DB_WORLD    "required_39_quest_variety_is_the_spice_of_death"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

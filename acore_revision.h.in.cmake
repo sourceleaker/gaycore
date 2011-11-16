8e8be56a2abfc0a9cbf8d815af588de6b5f6e34d
kiper <kiperek@o2.pl>
2011-11-16 15:30:36 +0100

    [100]Acore1:Commit:13d5b3813522f792e1500e926bbb3bacf6cff9a6
    * [A84] Fix quest Quiet the Cannons(25586&25585)

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "99"
+#define ACORE_REVISION "100"
 
-#define ACORE_DB_WORLD    "required_99_quest_24686"
+#define ACORE_DB_WORLD    "required_100_quest_25586"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

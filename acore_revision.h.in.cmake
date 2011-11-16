b31ae80aa6a9aa85e5c99aab1d6004818bab22ae
kiper <kiperek@o2.pl>
2011-11-16 17:39:09 +0100

    [132]Acore1:Commit:31c456f7dc3f4c8691e3a1964a51fbc8f163acba
    * [A104] Fix recive loot :)

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,6 +1,6 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "131"
+#define ACORE_REVISION "132"
 
 #define ACORE_DB_WORLD    "required_131_Quest_Poison_Water"
 #define ACORE_DB_CHAR     "required_03_db_version"

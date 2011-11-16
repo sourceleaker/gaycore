4e2d86373434f498f9774645d451ce8ee65b781b
kiper <kiperek@o2.pl>
2011-11-16 17:36:32 +0100

    [130]Acore:1Commit:45fa5a9af304354e09cab16a5a5a5316dcc1122d
    * HoO SQL

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "129"
+#define ACORE_REVISION "130"
 
-#define ACORE_DB_WORLD    "required_123_Ashenvale_NPC_SAI_Part1"
+#define ACORE_DB_WORLD    "required_130_instance_hoo"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

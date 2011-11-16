4439c28d28d6b50636ac8f60c8565396436d083e
kiper <kiperek@o2.pl>
2011-11-16 16:29:33 +0100

    [112]ACore1:Commit:0c30751b52e86aee416ecf334feb5f8de9ce51f5
    * [A96] Fix quest Still With The Zapped Giants (25465)

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "111"
+#define ACORE_REVISION "112"
 
-#define ACORE_DB_WORLD    "required_111_Ogre_Abduction"
+#define ACORE_DB_WORLD    "required_112_quest_25465"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

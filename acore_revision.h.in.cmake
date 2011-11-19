c31ba805e318fca88c495d62aa8f58654d8858f8
kiper <kiperek@o2.pl>
2011-11-19 11:31:35 +0100

    [167]Acore1:Commit:8c2743b1c00c7d8f387d63f91f60f4330d40f27f
    * [A137] Fix for quest Secure the Cargo!

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "166"
+#define ACORE_REVISION "167"
 
-#define ACORE_DB_WORLD    "required_166_q_24699"
+#define ACORE_DB_WORLD    "required_167_Secure_the_Cargo"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

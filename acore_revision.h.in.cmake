09da875ad8b9e985f2d8bc9ea5377337148a965e
kiper <kiperek@o2.pl>
2011-11-19 11:57:56 +0100

    [180]Acore1:Commit:eb4e819e79f0b20e79e58bc539732cb027973d09
    * [A149] Little fix for Art of a Druid

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "179"
+#define ACORE_REVISION "180"
 
-#define ACORE_DB_WORLD    "required_179_Stonesplinters"
+#define ACORE_DB_WORLD    "required_180_art_of_druid"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

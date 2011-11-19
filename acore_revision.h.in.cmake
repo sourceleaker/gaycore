1d201d9d72c07385ced5f5c2b1d6f27596322ace
kiper <kiperek@o2.pl>
2011-11-19 11:56:30 +0100

    [179]Acore1:Commit:bf20607f55fdb00b8c3d0351be705677c780782e
    * [A148] Dodanie poprawnych spawnów dla Stonesplinter Shaman oraz Stonesplinter Bonesnapper

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "178"
+#define ACORE_REVISION "179"
 
-#define ACORE_DB_WORLD    "required_178_herocall_board"
+#define ACORE_DB_WORLD    "required_179_Stonesplinters"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

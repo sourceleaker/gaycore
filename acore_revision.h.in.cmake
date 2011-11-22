fee079aeb07369cdf307fb486bcaff8f55e6606b
Jenova <Jenovas0@yahoo.com>
2011-11-23 00:31:35 +0100

    [222] UPDATED AllInOne - compile it and throw .exe into SQL  - Enjoy

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,6 +1,6 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "221"
+#define ACORE_REVISION "222"
 
 #define ACORE_DB_WORLD    "required_220_TanarisZone"
 #define ACORE_DB_CHAR     "required_03_db_version"
--- src/tools/atlantiss/AllInOne-SqlUpdates/AllInOne-SqlUpdates/Program.cs
+++ src/tools/atlantiss/AllInOne-SqlUpdates/AllInOne-SqlUpdates/Program.cs
@@ -11,25 +11,170 @@ namespace AllInOne_SqlUpdates
     {
         static void Main(string[] args)
         {
-            // Atlantiss Updates
-            // ---------------------------------
-            string path_A = "sql/ACORE3/updates/world";
-            Dictionary<float, string> Data_A = new Dictionary<float, string>();
-            
-            Regex Regex_A = new Regex(@"(?<number>\d{1,4})_(?<title>.*)");
-            DirectoryInfo dir = new DirectoryInfo(path_A);
-            FileInfo[] Files  = dir.GetFiles("*.sql");
-            Console.WriteLine("Atlantiss Updates generate, found files: {0}", Files.Count());
+            AtlantissWorld();
+            AtlantissCharacters();
+            AtlantissAuth();
 
-            foreach(FileInfo file in Files)
+            SkyfireWorld();
+            SkyfireCharacters();           
+            SkyfireAuth();
+
+            System.Threading.Thread.Sleep(15000);
+        }
+
+        static void AtlantissWorld()
+        {
+            // Atlantiss World Updates
+            // ---------------------------------
+            string path_A = "ACORE3/updates/world";
+            if (!Directory.Exists(path_A))
             {
-                Match data = Regex_A.Match(file.Name);
-                float key = Convert.ToSingle(data.Groups["number"].Value);
+                Console.WriteLine("Atlanitiss World Directory (ACORE3/updates/world) Not Found");
+                return;
+            }
+
+            Dictionary<float, string> Data_A = new Dictionary<float, string>();
+
+            Regex Regex_A = new Regex(@"(?<number>\d{1,4})_(?<title>.*)");
+            DirectoryInfo dirA = new DirectoryInfo(path_A);
+            FileInfo[] FilesA = dirA.GetFiles("*.sql");
+            Console.WriteLine("Atlantiss World Updates generate, found files: {0}", FilesA.Count());
+
+            foreach (FileInfo fileA in FilesA)
+            {
+                Match dataA = Regex_A.Match(fileA.Name);
+                float keyA = Convert.ToSingle(dataA.Groups["number"].Value);
 
                 while (true)
                 {
-                    if (Data_A.ContainsKey(key))
-                        key += 0.01f;
+                    if (Data_A.ContainsKey(keyA))
+                        keyA += 0.01f;
+                    else
+                        break;
+                }
+
+                StringBuilder tmpA = new StringBuilder();
+                tmpA.AppendFormat("-- @FILE: {0:S}", fileA.Name);
+                tmpA.AppendLine();
+                using (StreamReader f = fileA.OpenText())
+                {
+                    string l;
+                    while ((l = f.ReadLine()) != null)
+                        tmpA.AppendLine(l);
+                }
+
+                Data_A[keyA] = tmpA.ToString();
+            }
+
+            StringBuilder outputA = new StringBuilder();          
+            outputA.AppendFormat("-- Atlantiss World Sql updates: {0:S} files", FilesA.Count().ToString());
+            outputA.AppendLine();
+            outputA.AppendLine("-- ------------------------------------ ");
+            outputA.AppendLine(); outputA.AppendLine();
+            
+            List<float> keysA = new List<float>(Data_A.Keys);
+            keysA.Sort();
+            foreach (float keyA in keysA)
+            {
+                outputA.AppendLine(Data_A[keyA].ToString());
+            }
+
+            using (StreamWriter f = new StreamWriter("Atlantiss_world_updates.sql"))
+            {
+                f.Write(outputA.ToString());
+            }
+        }
+
+        static void AtlantissCharacters()
+        {
+            // Atlantiss Characters Updates
+            // ---------------------------------
+            string path_A = "ACORE3/updates/characters";
+            if (!Directory.Exists(path_A))
+            {
+                Console.WriteLine("Atlanitiss Characters Directory (ACORE3/updates/characters) Not Found");
+                return;
+            }
+
+            Dictionary<float, string> Data_A = new Dictionary<float, string>();
+
+            Regex Regex_A = new Regex(@"(?<number>\d{1,4})_(?<title>.*)");
+            DirectoryInfo dirA = new DirectoryInfo(path_A);
+            FileInfo[] FilesA = dirA.GetFiles("*.sql");
+            Console.WriteLine("Atlantiss Characters Updates generate, found files: {0}", FilesA.Count());
+
+            foreach (FileInfo fileA in FilesA)
+            {
+                Match dataA = Regex_A.Match(fileA.Name);
+                float keyA = Convert.ToSingle(dataA.Groups["number"].Value);
+
+                while (true)
+                {
+                    if (Data_A.ContainsKey(keyA))
+                        keyA += 0.01f;
+                    else
+                        break;
+                }
+
+                StringBuilder tmpA = new StringBuilder();
+                tmpA.AppendFormat("-- @FILE: {0:S}", fileA.Name);
+                tmpA.AppendLine();
+                using (StreamReader f = fileA.OpenText())
+                {
+                    string l;
+                    while ((l = f.ReadLine()) != null)
+                        tmpA.AppendLine(l);
+                }
+
+                Data_A[keyA] = tmpA.ToString();
+            }
+
+            StringBuilder outputA = new StringBuilder();
+            outputA.AppendFormat("-- Atlantiss Characters Sql updates: {0:S} files", FilesA.Count().ToString());
+            outputA.AppendLine();
+            outputA.AppendLine("-- ------------------------------------ ");
+            outputA.AppendLine(); outputA.AppendLine();
+
+            List<float> keysA = new List<float>(Data_A.Keys);
+            keysA.Sort();
+            foreach (float keyA in keysA)
+            {
+                outputA.AppendLine(Data_A[keyA].ToString());
+            }
+
+            using (StreamWriter f = new StreamWriter("Atlantiss_characters_updates.sql"))
+            {
+                f.Write(outputA.ToString());
+            }
+        }
+
+        static void AtlantissAuth()
+        {
+            // Atlantiss Auth Updates
+            // ---------------------------------
+            string path_A = "ACORE3/updates/auth";
+            if (!Directory.Exists(path_A))
+            {
+                Console.WriteLine("Atlanitiss Auth Directory (ACORE3/updates/auth) Not Found");
+                return;
+            }
+
+            Dictionary<float, string> Data_A = new Dictionary<float, string>();
+
+            Regex Regex_A = new Regex(@"(?<number>\d{1,4})_(?<title>.*)");
+            DirectoryInfo dirA = new DirectoryInfo(path_A);
+            FileInfo[] FilesA = dirA.GetFiles("*.sql");
+            Console.WriteLine("Atlantiss Auth Updates generate, found files: {0}", FilesA.Count());
+
+            foreach (FileInfo file in FilesA)
+            {
+                Match dataA = Regex_A.Match(file.Name);
+                float keyA = Convert.ToSingle(dataA.Groups["number"].Value);
+
+                while (true)
+                {
+                    if (Data_A.ContainsKey(keyA))
+                        keyA += 0.01f;
                     else
                         break;
                 }
@@ -44,25 +189,218 @@ namespace AllInOne_SqlUpdates
                         tmp.AppendLine(l);
                 }
 
-                Data_A[key] = tmp.ToString();
+                Data_A[keyA] = tmp.ToString();
             }
 
-            StringBuilder output = new StringBuilder();
-            output.AppendFormat("-- Atlantiss Sql updates: {0:S} files", Files.Count().ToString());
-            output.AppendLine();
-            output.AppendLine("-- ------------------------------------ ");
-            output.AppendLine(); output.AppendLine();
+            StringBuilder outputA = new StringBuilder();
 
-            List<float> keys = new List<float>(Data_A.Keys);
-            keys.Sort();
-            foreach (float key in keys)
+            outputA.AppendFormat("-- Atlantiss Auth Sql updates: {0:S} files", FilesA.Count().ToString());
+            outputA.AppendLine();
+            outputA.AppendLine("-- ------------------------------------ ");
+            outputA.AppendLine(); outputA.AppendLine();
+
+            List<float> keysA = new List<float>(Data_A.Keys);
+            keysA.Sort();
+            foreach (float key in keysA)
             {
-                output.AppendLine(Data_A[key].ToString());
+                outputA.AppendLine(Data_A[key].ToString());
             }
 
-            using (StreamWriter f = new StreamWriter("Atlantiss_updates.sql"))
+            using (StreamWriter f = new StreamWriter("Atlantiss_auth_updates.sql"))
             {
-                f.Write(output.ToString());
+                f.Write(outputA.ToString());
+            }
+        }
+
+        static void SkyfireWorld()
+        {
+            /// Skyfire World Updates
+            // ---------------------------------
+            string path_A = "updates/world";
+            if (!Directory.Exists(path_A))
+            {
+                Console.WriteLine("Skyfire World Directory (updates/world) Not Found");
+                return;
+            }
+
+            Dictionary<float, string> Data_A = new Dictionary<float, string>();
+
+            Regex Regex_A = new Regex(@"(?<number>\d{1,4})_(?<title>.*)");
+            DirectoryInfo dirA = new DirectoryInfo(path_A);
+            FileInfo[] FilesA = dirA.GetFiles("*.sql");
+            Console.WriteLine("Skyfire World Updates generate, found files: {0}", FilesA.Count());
+
+            foreach (FileInfo file in FilesA)
+            {
+                Match dataA = Regex_A.Match(file.Name);
+                float keyA = Convert.ToSingle(dataA.Groups["number"].Value);
+
+                while (true)
+                {
+                    if (Data_A.ContainsKey(keyA))
+                        keyA += 0.01f;
+                    else
+                        break;
+                }
+
+                StringBuilder tmp = new StringBuilder();
+                tmp.AppendFormat("-- @FILE: {0:S}", file.Name);
+                tmp.AppendLine();
+                using (StreamReader f = file.OpenText())
+                {
+                    string l;
+                    while ((l = f.ReadLine()) != null)
+                        tmp.AppendLine(l);
+                }
+
+                Data_A[keyA] = tmp.ToString();
+            }
+
+            StringBuilder outputA = new StringBuilder();
+
+            outputA.AppendFormat("-- Skyfire World Sql updates: {0:S} files", FilesA.Count().ToString());
+            outputA.AppendLine();
+            outputA.AppendLine("-- ------------------------------------ ");
+            outputA.AppendLine(); outputA.AppendLine();
+
+            List<float> keysA = new List<float>(Data_A.Keys);
+            keysA.Sort();
+            foreach (float key in keysA)
+            {
+                outputA.AppendLine(Data_A[key].ToString());
+            }
+
+            using (StreamWriter f = new StreamWriter("Skyfire_world_updates.sql"))
+            {
+                f.Write(outputA.ToString());
+            }
+        }
+
+        static void SkyfireCharacters()
+        {
+            /// Skyfire Characters Updates
+            // ---------------------------------
+            string path_A = "updates/characters";
+            
+            if (!Directory.Exists(path_A))
+            {
+                Console.WriteLine("Skyfire Characters Directory (updates/characters) Not Found");
+                return;
+            }
+            Dictionary<float, string> Data_A = new Dictionary<float, string>();
+
+            Regex Regex_A = new Regex(@"(?<number>\d{1,4})_(?<title>.*)");
+            DirectoryInfo dirA = new DirectoryInfo(path_A);
+            FileInfo[] FilesA = dirA.GetFiles("*.sql");
+            Console.WriteLine("Skyfire Characters Updates generate, found files: {0}", FilesA.Count());
+
+            foreach (FileInfo file in FilesA)
+            {
+                Match dataA = Regex_A.Match(file.Name);
+                float keyA = Convert.ToSingle(dataA.Groups["number"].Value);
+
+                while (true)
+                {
+                    if (Data_A.ContainsKey(keyA))
+                        keyA += 0.01f;
+                    else
+                        break;
+                }
+
+                StringBuilder tmp = new StringBuilder();
+                tmp.AppendFormat("-- @FILE: {0:S}", file.Name);
+                tmp.AppendLine();
+                using (StreamReader f = file.OpenText())
+                {
+                    string l;
+                    while ((l = f.ReadLine()) != null)
+                        tmp.AppendLine(l);
+                }
+
+                Data_A[keyA] = tmp.ToString();
+            }
+
+            StringBuilder outputA = new StringBuilder();
+
+            outputA.AppendFormat("-- Skyfire Characters Sql updates: {0:S} files", FilesA.Count().ToString());
+            outputA.AppendLine();
+            outputA.AppendLine("-- ------------------------------------ ");
+            outputA.AppendLine(); outputA.AppendLine();
+
+            List<float> keysA = new List<float>(Data_A.Keys);
+            keysA.Sort();
+            foreach (float key in keysA)
+            {
+                outputA.AppendLine(Data_A[key].ToString());
+            }
+
+            using (StreamWriter f = new StreamWriter("Skyfire_characters_updates.sql"))
+            {
+                f.Write(outputA.ToString());
+            }
+        }
+
+        static void SkyfireAuth()
+        {
+            /// Skyfire Auth Updates
+            // ---------------------------------
+            string path_A = "updates/auth";
+            if (!Directory.Exists(path_A))
+            {
+                Console.WriteLine("Skyfire Auth Directory (updates/auth) Not Found");
+                return;
+            }
+
+            Dictionary<float, string> Data_A = new Dictionary<float, string>();
+
+            Regex Regex_A = new Regex(@"(?<number>\d{1,4})_(?<title>.*)");
+            DirectoryInfo dirA = new DirectoryInfo(path_A);
+            FileInfo[] FilesA = dirA.GetFiles("*.sql");
+            Console.WriteLine("Skyfire Auth Updates generate, found files: {0}", FilesA.Count());
+
+            foreach (FileInfo file in FilesA)
+            {
+                Match dataA = Regex_A.Match(file.Name);
+                float keyA = Convert.ToSingle(dataA.Groups["number"].Value);
+
+                while (true)
+                {
+                    if (Data_A.ContainsKey(keyA))
+                        keyA += 0.01f;
+                    else
+                        break;
+                }
+
+                StringBuilder tmp = new StringBuilder();
+                tmp.AppendFormat("-- @FILE: {0:S}", file.Name);
+                tmp.AppendLine();
+                using (StreamReader f = file.OpenText())
+                {
+                    string l;
+                    while ((l = f.ReadLine()) != null)
+                        tmp.AppendLine(l);
+                }
+
+                Data_A[keyA] = tmp.ToString();
+            }
+
+            StringBuilder outputA = new StringBuilder();
+
+            outputA.AppendFormat("-- Skyfire Auth Sql updates: {0:S} files", FilesA.Count().ToString());
+            outputA.AppendLine();
+            outputA.AppendLine("-- ------------------------------------ ");
+            outputA.AppendLine(); outputA.AppendLine();
+
+            List<float> keysA = new List<float>(Data_A.Keys);
+            keysA.Sort();
+            foreach (float key in keysA)
+            {
+                outputA.AppendLine(Data_A[key].ToString());
+            }
+
+            using (StreamWriter f = new StreamWriter("Skyfire_auth_updates.sql"))
+            {
+                f.Write(outputA.ToString());
             }
         }
     }

446a8d7b84c7a8885254134bbb377f31788fc27e
kiper <kiperek@o2.pl>
2011-11-22 11:21:59 +0100

    [208]Acore1:Commit:c1bd4fb9957b65c5e860d658e9c3e88d460f6f9f
    * Fixed Shaman Tauren Language/Spell and Others problems

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "207"
+#define ACORE_REVISION "208"
 
-#define ACORE_DB_WORLD    "required_207_Winterspring_QuestLine"
+#define ACORE_DB_WORLD    "required_208_ShamanTauren_PCIS"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

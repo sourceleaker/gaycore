61821a9727a321fdcb23317601da905a33ff5329
kiper <kiperek@o2.pl>
2011-11-24 13:37:43 +0100

    [223]Fix quest Extinguish the Fires(26078)

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "222"
+#define ACORE_REVISION "223"
 
-#define ACORE_DB_WORLD    "required_220_TanarisZone"
+#define ACORE_DB_WORLD    "required_223_quest_26078"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

1b1c7dbc5eea78ff15520d53202241d63035b5fa
kiper <kiperek@o2.pl>
2011-11-24 13:40:50 +0100

    [224] To powinno poprawic problem z freezem postaci po kliknieciu na Defiant Troll

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "223"
+#define ACORE_REVISION "224"
 
-#define ACORE_DB_WORLD    "required_223_quest_26078"
+#define ACORE_DB_WORLD    "required_224_npc_34830"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

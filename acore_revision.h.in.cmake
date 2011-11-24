bec85a2888d5e33b3de421fe058d28f349bdcf5a
Jenova <Jenovas0@yahoo.com>
2011-11-24 23:41:15 +0100

    Revision Error Fix

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -2,7 +2,7 @@
 #define __ACORE_REVISION_H__
 #define ACORE_REVISION "226"
 
-#define ACORE_DB_WORLD    "required_225_Deposits_add_at_Uldum_Deepholm_Lost_Isles"
+#define ACORE_DB_WORLD    "required_225_Deposits_add_at_Uldum_Deepholm_LostIsles"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

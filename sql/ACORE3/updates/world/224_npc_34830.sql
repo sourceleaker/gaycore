19988f222b928f5ac7b60ef6853ae5291f2d5516
kiper <kiperek@o2.pl>
2011-11-24 13:43:15 +0100

    Fix syntax error :<

--- sql/ACORE3/updates/world/224_npc_34830.sql
+++ sql/ACORE3/updates/world/224_npc_34830.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `world_db_version` CHANGE  `required_223_quest_26078` `required_224_npc_34830`  BIT(1);
 
-UPDATE `creature_template_addon``auras`='45111' WHERE `entry`=34830;
+UPDATE `creature_template_addon` SET `auras`='45111' WHERE `entry`=34830;

006dbde370f8902ea62c3371a3cda88b991bffb2
Bastek <bastol23@poczta.fm>
2011-11-24 22:41:39 +0100

    [226] Deposits add at Uldum Deepholm Lost Isles

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "225"
+#define ACORE_REVISION "226"
 
-#define ACORE_DB_WORLD    "required_224_npc_34830"
+#define ACORE_DB_WORLD    "required_225_Deposits_add_at_Uldum_Deepholm_Lost_Isles"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

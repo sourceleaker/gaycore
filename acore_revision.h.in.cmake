d33e28ec5a699e3919b2ca92c2856e3a331a5060
Bastek <bastol23@poczta.fm>
2011-11-25 22:46:48 +0100

    [238] Removal of old Wetlands NPC's

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "237"
+#define ACORE_REVISION "238"
 
-#define ACORE_DB_WORLD    "required_236_Questrelation_added_to_GO"
+#define ACORE_DB_WORLD    "required_237_Removal_of_old_Wetlands_NPCs"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

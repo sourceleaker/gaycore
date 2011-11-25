ac99aeba45681caf0ad90de8f78a9e7967fc7f60
Bastek <bastol23@poczta.fm>
2011-11-25 23:24:54 +0100

    Revision fix

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "238"
+#define ACORE_REVISION "239"
 
-#define ACORE_DB_WORLD    "required_237_Removal_of_old_Wetlands_NPCs"
+#define ACORE_DB_WORLD    "required_239_synch_revision"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

ebf341c3709af1d12daf37c09d79643d7acc5a13
kiper <kiperek@o2.pl>
2011-12-05 12:14:25 +0100

    [247]Drop field deathstate from creature (orginally remove by TC)

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "246"
+#define ACORE_REVISION "247"
 
-#define ACORE_DB_WORLD    "required_246_quest_28415"
+#define ACORE_DB_WORLD    "required_247_creature"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__
---
+++ sql/ACORE3/updates/world/247_creature.sql
@@ -0,0 +1,3 @@
+ ALTER TABLE `world_db_version` CHANGE  `required_246_quest_28415` `required_247_creature` BIT(1); 
+ 
+ ALTER TABLE `creature` DROP COLUMN `DeathState`;

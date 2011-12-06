645266621f6e6352b31c647c3bcd94d25ae14fb4
kiper <kiperek@o2.pl>
2011-12-06 12:35:38 +0100

    [253]Searing Gorge texty do questow + chain

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,8 +1,8 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "252"
+#define ACORE_REVISION "253"
 
-#define ACORE_DB_WORLD    "required_252_quest_26043"
+#define ACORE_DB_WORLD    "required_253_searing_gorge"
 #define ACORE_DB_CHAR     "required_03_db_version"
 #define ACORE_DB_AUTH     "required_03_db_version"
 #endif // __ACORE_REVISION_H__

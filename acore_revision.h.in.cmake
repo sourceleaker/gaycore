b3f438f20a387d2b042addf7f5be163cc1a59974
Jenova <Jenovas0@yahoo.com>
2011-12-07 23:28:48 +0100

    [258] Spell&Talent Fixes - Inner Focus, Frozen Power, Cut&Chase, Euphoria (removed hack), Grand Crusader, Imprv Frost Presence, Imprv Soul Fire, Imprv Mind Blast, Mind Trauma, Nether Vortex

--- acore_revision.h.in.cmake
+++ acore_revision.h.in.cmake
@@ -1,6 +1,6 @@
 #ifndef __ACORE_REVISION_H__
 #define __ACORE_REVISION_H__
-#define ACORE_REVISION "257"
+#define ACORE_REVISION "258"
 
 #define ACORE_DB_WORLD    "required_257_creature_text"
 #define ACORE_DB_CHAR     "required_03_db_version"
